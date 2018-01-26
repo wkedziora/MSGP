2038524824b15216599b31694d86804d847384af
Wojciech Kedziora <wojciech.kedziora@wl.sggw.pl>
2018-01-26 15:42:38 +0100

    polishing

--- data_manipulation.R
+++ data_manipulation.R
@@ -37,7 +37,7 @@ add_gps <- function(dataset, col_name = "gat") {
 # function for fast map drawing
 draw_map <- function(dataframe, facet = FALSE) {
   map <- tm_shape(Europe, bbox = "Poland", projection="longlat", is.master = TRUE) + tm_borders() + # Polska or Europe for performance
-    tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 4) +
+    # tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 4) +
     qtm(dataframe, dots.alpha = 0.5) 
     # tm_compass(position = c("left", "bottom")) +
     # tm_scale_bar(position = c("left", "bottom")) + 
@@ -50,9 +50,9 @@ draw_map <- function(dataframe, facet = FALSE) {
 draw_maps_4 <- function(dataframe, x, facet = FALSE){
   dataframe %>% filter(gat == x) -> subset
   map <- tm_shape(Europe, bbox = "Poland", projection="longlat", is.master = TRUE) + tm_borders() +
-    tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 3) +
+    # tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 3) +
     qtm(subset, dots.alpha = 0.5) + 
-    tm_layout(asp = 0, outer.margins=0)
+    tm_layout(asp = 0, outer.margins = 0)
   if (facet == TRUE) 
     map + tm_facets(by = "group", free.coords = TRUE, drop.units = TRUE, drop.empty.facets = FALSE)
   else 
@@ -81,9 +81,8 @@ draw_density_plot <- function(dataframe, x) {
   rr <- mask(r, as(poland, "Spatial"))
   map <- tm_shape(rr) + tm_raster("layer", breaks = seq(0, 0.1, by=0.02),  legend.show = TRUE) +
     tm_shape(poland) + tm_borders() +
-    tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 3) +
-    tm_shape(subset) + tm_dots(alpha = 0.1) + 
-    tm_layout(legend.position = c("LEFT", "BOTTOM"))
+    # tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 3) +
+    tm_shape(subset) + tm_dots(alpha = 0.1) 
   return(map)
 }
 # draw_density_plot(sites_so_gps, "BK")
@@ -158,6 +157,10 @@ bind_rows("< 0.5m" = trees_05,
   filter(n() > 99) %>% #removes species that are seldom
   ungroup() -> trees_all
 
+trees_all_gps <- add_gps(trees_all)
+draw_map(trees_all_gps)
+n_distinct(trees_all$nr_punktu)
+
 # I need to add age restriction to avoid young pine monocultures
 sites %>% select(nr_punktu, nr_podpow, gat_pan_pr, wiek_pan_pr) %>% left_join(trees_all %>% select(-nr_punktu), by = "nr_podpow") -> sites_so
 
@@ -165,22 +168,30 @@ sites_so_gps <- add_gps(sites_so)
 sites_so_gps$gat <- factor(sites_so_gps$gat)
 sites_so_gps$group <- factor(sites_so_gps$group)
 
+draw_map(sites_so_gps)
+n_distinct(sites_so_gps$nr_punktu)
+
 table(cut(sites_so$wiek_pan_pr, breaks = c(0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240)), sites_so$group)
 
 ### Species: Fagus sylvatica ------------------------------------------------------------------------------------------
-gatunki <- c("BK", "JD", "ŚW", "GB")
-names(gatunki) <- gatunki
-
 buk <- read_shape("ranges/Fagus_sylvatica_EUFORGEN.shp", as.sf = TRUE) %>% st_make_valid() 
-buk_clip <- st_intersection(poland, buk)
-buk_zasieg <- read_shape("dane/zasiegi_gatunkow_simplified.shp", as.sf = TRUE, current.projection = "2180") %>% filter(GAT == "Bk")
+buk_zasieg_euforgen <- st_intersection(poland, buk)
+buk_zasieg_naturalny <- read_shape("dane/zasiegi_gatunkow_simplified.shp", as.sf = TRUE, current.projection = "2180") %>% 
+  filter(GAT == "Bk" & OPIS == "granica zasiďż˝gu naturalnego")
+buk_zasieg_antropogeniczny <- read_shape("dane/zasiegi_gatunkow_simplified.shp", as.sf = TRUE, current.projection = "2180") %>% 
+  filter(GAT == "Bk" & OPIS == "granica zasiďż˝gu antropogenicznego (wg S. Tarasiuka, 1992)")
 
+draw_maps_4(sites_so_gps, "BK", facet = FALSE)
 draw_maps_4(sites_so_gps, "BK", facet = TRUE)
-density_plot_bk <- draw_density_plot(sites_so_gps, "BK") 
-density_plot_bk + tm_shape(buk_clip) + tm_fill(col = "blue", alpha = 0.6)
 
-tm_shape(buk_clip) + tm_fill(col = "blue", alpha = 0.6) +
-  tm_shape(buk_zasieg) + tm_lines()
+density_plot_bk <- draw_density_plot(sites_so_gps, "BK") 
+map1 <- density_plot_bk
+map2 <- density_plot_bk + tm_shape(buk_zasieg_euforgen) + tm_polygons(col = "grey", alpha = 0.05, lty = "longdash") 
+map3 <- density_plot_bk + tm_shape(buk_zasieg_naturalny) + tm_lines(col = "black", lty = "dashed")
+map4 <- density_plot_bk + tm_shape(buk_zasieg_antropogeniczny) + tm_lines(col = "red", lty = "dotdash")
+  
+tmap_arrange(map1, map2, map3, map4, asp = NA) + 
+  tm_layout(legend.position = c("LEFT", "BOTTOM"))
 
 
 # many species at once --- --- ---
